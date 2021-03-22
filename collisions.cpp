cdadfa4e2e49b4310cdcadb23431742ad653ecc6
kaij <kaijamesoncs@gmail.com>
2021-03-22 12:09:10 -0400

    added some basic comments/info to the file

--- collisions.cpp
+++ collisions.cpp
@@ -10,13 +10,33 @@
 #include <vector>
 #define MCW MPI_COMM_WORLD
 using namespace std;
+
+struct Satellite {
+    int perigee;
+    int apogee;
+};
+
 int main(int argc, char **argv){
   MPI_Init(&argc, &argv);
   int rank, size;
   MPI_Status mystatus;
   MPI_Comm_rank(MCW, &rank); 
   MPI_Comm_size(MCW, &size); 
+  if (rank == 0) {
+    //determine perigee and apogee for each satellite
+    //can generate 2 numbers between 100 and 160 and just call the larger number apogee, smaller number perigee
+  }
+  else {
+    //receive the satellite information from rank 0
+    
+    //create satellite objects
 
+    //using this as a guide for determing update information https://www.orbiter-forum.com/threads/so-you-want-to-calculate-orbits.24431/
+
+
+
+
+  }
   MPI_Finalize();
 
   return 0;
